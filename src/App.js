9759352a66d0ed8457d146107f304d2c8f928b9b
raymondvooo <raymondvooo@gmail.com>
2019-03-07 12:43:23 -0800

    buttons created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          React Calculator App
         </header>
+        <div className="calc-container">
+        <div className="row">
+          <button className="controls">AC</button>
+          <button className="controls">(</button>
+          <button className="controls">)</button>
+          <button className="controls">+/-</button>
+          <button className="operators">/</button>
+        </div>
+         <div className="row">
+          <button>sin</button>
+          <button className="numbers">7</button>
+          <button className="numbers">8</button>
+          <button className="numbers">9</button>
+          <button className="operators">*</button>
+        </div>
+         <div className="row">
+          <button>cos</button>
+          <button className="numbers">4</button>
+          <button className="numbers">5</button>
+          <button className="numbers">6</button>
+          <button className="operators">-</button>
+        </div>
+         <div className="row">
+          <button>tan</button>
+          <button className="numbers">1</button>
+          <button className="numbers">2</button>
+          <button className="numbers">3</button>
+          <button className="operators">+</button>
+        </div>
+        <div className="row">
+          <button>π</button>
+          <button className="numbers" id="zero">0</button>
+          <button className="numbers">.</button>
+          <button className="operators">=</button>
+        </div>
+        
+        
+        </div>
+
       </div>
     );
   }
